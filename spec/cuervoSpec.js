3e5e873747dcc4244aad01398088b4bb20583c5c
Nathan <natedsaint@gmail.com>
2015-05-15 14:12:52 -0700

    Modifying text to be more helpful and smaller, and beginning work on testing. Also adding jshintrc for node-style code.

--- spec/cuervoSpec.js
+++ spec/cuervoSpec.js
@@ -7,15 +7,25 @@ var Cuervo = require("../lib/cuervo.js"),
 describe("cuervo", function() {
     it("exists", function() {
         expect(Cuervo).toBeTruthy();
+        
     });
+
     it("can be instantiated", function() {
         expect(app).toBeTruthy();
         expect(app instanceof Cuervo).toBe(true);
     });
-    
-    describe("app.defaults", function() {
+
+    it("calls printCuervo on instantiation", function() {
+        var newApp;
+        spyOn(Cuervo.prototype,'printCuervo');
+        newApp = new Cuervo(args);
+        expect(Cuervo.prototype.printCuervo).toHaveBeenCalled();
+        expect(newApp instanceof Cuervo).toBe(true);
+    });
+    describe("cuervo.defaults", function() {
         it("exists", function() {
             expect(typeof app.defaults).toBe("object");
         });
     });
+
 });
